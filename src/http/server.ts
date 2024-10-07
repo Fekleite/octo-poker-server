2e27a9f42124c87c4201fdf124368879c3ed96c4
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-10-07 15:07:16 -0300

    feat: create join room event

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,17 +2,22 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 
+import { join } from '../controllers/RoomController';
+
 const app = express()
 const server = createServer(app);
 
-const io = new Server(server, {
+export const io = new Server(server, {
   cors: {
     origin: ['http://localhost:5173']
   }
 });
 
 io.on('connection', (socket) => {
-  console.log('Connected user', socket.id);
+  socket.on('join-room', ({ roomCode, username }: { roomCode: string; username: string }) => {
+    const payload = { roomCode, username }
+    join({ socket, payload })
+  })
 });
 
 server.listen(3333, () => {
