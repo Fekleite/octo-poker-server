81629f4f62a3ac8da923afd156b1f34a2e63f7c5
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-10-07 17:34:10 -0300

    refactor: use class on controller

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -1,47 +1,88 @@
 import { Socket } from "socket.io";
 import { io } from "../http/server";
+import { CloseEvent, CreateEvent, JoinEvent, LeaveEvent, Room } from "../@types/room";
 
-interface JoinRequest {
-  payload: {
-    roomCode: string; 
-    username: string
-  },
-  socket: Socket
+interface RoomsByCode {
+  [key: string]: Room;
 }
+class RoomController {
+  rooms: RoomsByCode = {}
 
-interface RemoveUsersRequest {
-  socket: Socket
-}
-
-interface RoomUser {
-  id: string;
-  username: string;
-}
-
-interface Rooms {
-  [key: string]: RoomUser[];
-}
-
-const rooms: Rooms = {}
-
-export function join({ socket, payload }: JoinRequest) {
-  const { roomCode, username } = payload;
-
-  socket.join(roomCode)
-
-  if (!rooms[roomCode]) {
-    rooms[roomCode] = []
+  create({ socket, payload }: CreateEvent) {
+    const { room, user } = payload;
+  
+    socket.join(room.code)
+  
+    if (!this.rooms[room.code]) {
+      this.rooms[room.code] = {
+        name: room.name,
+        code: room.code,
+        users: []
+      }
+    }
+  
+    this.rooms[room.code].users.push({ 
+      id: socket.id,
+      name: user.name,
+      role: 'admin'
+    })
+  
+    io.to(room.code).emit('room-users', this.rooms[room.code]);
   }
 
-  rooms[roomCode].push({ id: socket.id, username });
+  join({ socket, payload }: JoinEvent) {
+    const { room, user } = payload;
+  
+    socket.join(room.code)
+  
+    if (!this.rooms[room.code]) {
+      socket.emit('error', { error: "This room doesn't exists." })
+    }
+  
+    this.rooms[room.code].users.push({ 
+      id: socket.id, 
+      name: user.name,
+      role: 'default'
+    });
+  
+    io.to(room.code).emit('room-users', this.rooms[room.code]);
+  }
 
-  io.to(roomCode).emit('room-users', rooms[roomCode]);
-}
+  leave({ socket, payload }: LeaveEvent) {
+    const { room, user } = payload
 
-export function removeUsers({ socket }: RemoveUsersRequest) {
-  for (const roomCode in rooms) {
-    rooms[roomCode] = rooms[roomCode].filter((user) => user.id !== socket.id);
+    socket.leave(room.code)
 
-    io.to(roomCode).emit('room-users', rooms[roomCode]);
+    this.rooms[room.code].users.filter(u => u.id !== user.id)
+
+    io.to(room.code).emit('room-users', this.rooms[room.code]);
+  }
+
+  close({ socket, payload }: CloseEvent) {
+    const { room, user } = payload
+
+    const isAdminUser = this.rooms[room.code].users.find(u => u.id === user.id && u.role === 'admin')
+
+    if (!isAdminUser) {
+      socket.emit('error', { error: "You must be an admin to close a room." })
+    }
+
+    const roomThatWillBeClosed = io.sockets.adapter.rooms.get(room.code)
+
+    if (!roomThatWillBeClosed) {
+      socket.emit('error', { error: "This room doesn't exists." })
+    }
+
+    roomThatWillBeClosed?.forEach((socketId) => {
+      const socketToLeave = io.sockets.sockets.get(socketId);
+
+      if (socketToLeave) {
+        socketToLeave.leave(room.code)
+      }
+    })
+
+    delete this.rooms[room.code];
   }
 }
+
+export default new RoomController;
--- src/http/server.ts
+++ src/http/server.ts
@@ -2,7 +2,9 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 
-import { join, removeUsers } from '../controllers/RoomController';
+import { CloseEventBody, CreateEventBody, JoinEventBody, LeaveEventBody } from '../@types/room';
+import RoomController from '../controllers/RoomController';
+
 import { sendVote } from '../controllers/UserController';
 
 const app = express()
@@ -15,20 +17,25 @@ export const io = new Server(server, {
 });
 
 io.on('connection', (socket) => {
-  socket.on('join-room', ({ roomCode, username }: { roomCode: string; username: string }) => {
-    const payload = { roomCode, username }
-    
-    join({ socket, payload })
+  // Room
+  socket.on('create-room', (body: CreateEventBody) => {
+    RoomController.create({ socket, payload: body })
+  })
+  socket.on('join-room', (body: JoinEventBody) => {
+    RoomController.join({ socket, payload: body })
+  })
+  socket.on('leave-room', (body: LeaveEventBody) => {
+    RoomController.leave({ socket, payload: body })
+  })
+  socket.on('close-room', (body: CloseEventBody) => {
+    RoomController.close({ socket, payload: body })
   })
 
+  // USer
   socket.on('send-vote', ({ roomCode, vote }: { roomCode: string; vote: string }) => {
     const payload = { roomCode, vote }
 
     sendVote({ socket, payload })
-  })
-
-  socket.on('disconnect', () => {
-    removeUsers({ socket })
   })
 });
 
