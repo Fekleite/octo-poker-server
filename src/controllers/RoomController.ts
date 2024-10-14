ddc7122c11864c97e37a3e6ef96a174da7b9333b
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-10-14 17:06:51 -0300

    refactor: remove errors emitters

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -27,7 +27,7 @@ class RoomController {
       role: 'admin'
     })
   
-    io.to(room.code).emit('on-room-was-create', { room: this.rooms[room.code] });
+    io.to(room.code).emit('created', { room: this.rooms[room.code] });
   }
 
   join({ socket, payload }: JoinEvent) {
@@ -35,12 +35,6 @@ class RoomController {
   
     socket.join(room.code)
   
-    if (!this.rooms[room.code]) {
-      socket.emit('error', { error: "This room doesn't exists." })
-
-      return;
-    }
-
     const newUser: User = { 
       id: socket.id, 
       name: user.name,
@@ -49,7 +43,7 @@ class RoomController {
   
     this.rooms[room.code].users.push(newUser);
   
-    io.to(room.code).emit('on-user-joined-room', { 
+    io.to(room.code).emit('joined', { 
       user: newUser,
       room: this.rooms[room.code]
     });
@@ -59,41 +53,21 @@ class RoomController {
     const { room, user } = payload
 
     const userLeftRoom = this.rooms[room.code].users.find(u => u.id === user.id)
-
-    if (!userLeftRoom) {
-      socket.emit('error', { error: "This user isn't in this room!" })
-
-      return;
-    }
     
     socket.leave(room.code)
 
     this.rooms[room.code].users.filter(u => u.id !== user.id)
 
-    io.to(room.code).emit('on-user-left-room', { 
+    io.to(room.code).emit('left', { 
       user: userLeftRoom, 
       room: this.rooms[room.code]
     });
   }
 
   close({ socket, payload }: CloseEvent) {
-    const { room, user } = payload
-
-    const adminUser = this.rooms[room.code].users.find(u => u.id === user.id && u.role === 'admin')
-
-    if (!adminUser) {
-      socket.emit('error', { error: "You must be an admin to close a room." })
-
-      return;
-    }
+    const { room } = payload
 
     const roomThatWillBeClosed = io.sockets.adapter.rooms.get(room.code)
-
-    if (!roomThatWillBeClosed) {
-      socket.emit('error', { error: "This room doesn't exists." })
-
-      return;
-    }
 
     roomThatWillBeClosed?.forEach((socketId) => {
       const socketToLeave = io.sockets.sockets.get(socketId);
@@ -105,9 +79,7 @@ class RoomController {
 
     delete this.rooms[room.code];
 
-    io.to(room.code).emit('on-room-was-close', { 
-      user: adminUser,
-    });
+    io.to(room.code).emit('closed');
   }
 }
 
--- src/controllers/VoteController.ts
+++ src/controllers/VoteController.ts
@@ -20,50 +20,30 @@ class VoteController {
       value,
     })
 
-    io.to(room.code).emit('on-vote-was-send', { user: socket.id })
+    io.to(room.code).emit('vote-sent', { user: socket.id, value })
   }
 
   remove({ socket, payload }: RemoveEvent) {
     const { room } = payload
 
-    if (!this.votes[room.code]) {
-      socket.emit('error', { error: "This room doesn't exists!" })
-
-      return;
-    }
-
     const otherVotes = this.votes[room.code].filter(vote => vote.user !== socket.id)
     this.votes[room.code] = [...otherVotes]
 
-    io.to(room.code).emit('on-vote-was-remove', { user: socket.id })
+    io.to(room.code).emit('vote-remove')
   }
 
   reveal({ socket, payload }: RevealEvent) {
     const { room } = payload
-
-    if (!this.votes[room.code]) {
-      socket.emit('error', { error: "There isn't votes in this room!" })
-
-      return;
-    }
-
-    const roomVotes = this.votes[room.code]
     
-    io.to(room.code).emit('on-votes-were-reveal', { votes: roomVotes })
+    io.to(room.code).emit('revealed-votes')
   }
 
   reset({ socket, payload }: ResetEvent) {
     const { room } = payload
 
-    if (!this.votes[room.code]) {
-      socket.emit('error', { error: "This room doesn't exists!" })
-
-      return;
-    }
-
     this.votes[room.code] = []
 
-    io.to(room.code).emit('on-votes-were-reset', { votes: [] })
+    io.to(room.code).emit('reset-votes')
   }
 }
 
